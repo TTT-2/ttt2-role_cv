558933eabd2c072aa8f2c209e78e6714e59bba6f
Alessandro Levante <62677560+PinoMartirio@users.noreply.github.com>
2020-04-01 19:48:08 +0200

    Added Italian translation
    Added full Italian translation for the role Clairvoyant.

---
+++ lua/lang/italian/clairvoyant.lua
@@ -0,0 +1,13 @@
+L = LANG.GetLanguageTableReference("italian")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[CLAIRVOYANT.name] = "Chiaroveggente"
+L["info_popup_" .. CLAIRVOYANT.name] = [[Sei il Chiaroveggente!
+Manda tutti contro gli altri con la tua abilità!
+Non parlare troppo della tua abilità, altrimenti la pagherai subito!]]
+L["body_found_" .. CLAIRVOYANT.abbr] = "Era un Chiaroveggente."
+L["search_role_" .. CLAIRVOYANT.abbr] = "Questa persona era un Chiaroveggente!"
+L["target_" .. CLAIRVOYANT.name] = "Chiaroveggente"
+L["ttt2_desc_" .. CLAIRVOYANT.name] = [[Il Chiaroveggente può vedere se un giocatore è innocente o ha un ruolo speciale.
+Il suo obiettivo è di sopravvivere ai traditori come innocente.
+Sei l'unico che può uccidere il Jester e uccidendolo lo trasformerai in tuo compagno.]]

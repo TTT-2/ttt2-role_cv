002da6fb4d1911e20592dd8c03272aa0f76659d7
Tekiad <tekiadp@gmail.cm>
2020-07-31 00:44:58 -0300

    Create clairvoyant.lua
    spanish clairvoyant role translation

---
+++ lua/lang/spanish/clairvoyant.lua
@@ -0,0 +1,14 @@
+L = LANG.GetLanguageTableReference("spanish")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[CLAIRVOYANT.name] = "Clarividente"
+L["info_popup_" .. CLAIRVOYANT.name] = [[¡Eres el Clarividente!
+¡Mueve las piezas a tu favor con tu conocimiento!
+¡No hables demasiado de tu habilidad, ya que rápidamente querrán acabar contigo!]]
+L["body_found_" .. CLAIRVOYANT.abbr] = "¡Era un Clarividente!"
+L["search_role_" .. CLAIRVOYANT.abbr] = "Esta persona era un Clarividente."
+L["target_" .. CLAIRVOYANT.name] = "Clarividente"
+L["ttt2_desc_" .. CLAIRVOYANT.name] = [[El Clarividente es capaz de descifrar si una persona es inocente o tiene un rol especial.
+Su objetivo es sobrevivir a los traidores en conjunto a los inocentes.
+
+En combinación con el rol SOCIO y el de JESTER, puedes asesinar al Jester únicamente y llevarte un Socio gratis.]]

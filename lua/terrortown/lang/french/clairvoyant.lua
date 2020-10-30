d23c941ce1a86819cc1c030b7fd45e03f06fa5b3
MisterClems <73431903+MisterClems@users.noreply.github.com>
2020-10-30 21:46:27 +0100

    Create clairvoyant.lua (#6)
    french translation :Clairvoyant

---
+++ lua/terrortown/lang/french/clairvoyant.lua
@@ -0,0 +1,13 @@
+local L = LANG.GetLanguageTableReference("french")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[CLAIRVOYANT.name] = "Clairvoyant"
+L["info_popup_" .. CLAIRVOYANT.name] = [[Vous êtes le Clairvoyant!
+Retournez tout le monde les uns contre les autres grâce à vos connaisance!
+Ne parlez pas trop de votre capacités, sinon vous le paierez rapidement!]]
+L["body_found_" .. CLAIRVOYANT.abbr] = "C'était un Clairvoyant."
+L["search_role_" .. CLAIRVOYANT.abbr] = "Cette personne était un Clairvoyant!"
+L["target_" .. CLAIRVOYANT.name] = "Clairvoyant"
+L["ttt2_desc_" .. CLAIRVOYANT.name] = [[Le Clairvoyant est capable de voir si un joueur est innocent ou si un joueur a un rôle particulier.
+Son but est de survivre aux traîtres en tant qu'innocent.
+En combinaison avec le rôle d'Acolyte et le rôle du Bouffon, vous êtes le seul à pouvoir tuer le bouffon, et en le tuant, vous le transformerez en Acolyte.]]
